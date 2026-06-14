0361099708062cc5ad7e5e80f7b1e4034630377b
Sacchu-1206 <sachinyadav692003@gmail.com>
2026-06-14 15:32:17 +0530

    Create project pages and folder structure

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+function Home() {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-4xl font-bold">
+        Frontend Internship Project
+      </h1>
+
+      <p className="mt-4">
+        Translator and Random String Generator
+      </p>
+    </div>
+  );
+}
+
+export default Home;
